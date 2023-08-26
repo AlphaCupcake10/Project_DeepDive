237e6d7f09b76a8b940f42c9e4b3ab8c8993daa3
AlphaCupcake10 <alphacupcake@outlook.com>
2023-08-26 15:25:41 +0530

    Added Crate

--- Assets/Scripts/GunConfig.cs
+++ Assets/Scripts/GunConfig.cs
@@ -3,14 +3,18 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/Gun", order = 1)]
 public class GunConfig : ScriptableObject
 {
+    public Sprite graphic;
     public GunConfigSingle[] FireModes;
 }
 [System.Serializable]
 public class GunConfigSingle
 {
     public float FireTimer = 0;//0 means rapid
+    [HideInInspector]
+    public float CurrentTimer = 0;//0 means rapid
     public float Speed = 5f;
     public float DestroyTime=5f;
     public float Damage = 10f;
     public float NormalForce = 25;
+    public GameObject CustomBullet;
 }
