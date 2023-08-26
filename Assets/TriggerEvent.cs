534eed19381c5ffbaeb8913b2575049defc05f38
Srishti <srishtichauhanchauhan1@gmail.com>
2023-08-26 23:18:44 +0530

    trigger

--- Assets/TriggerEvent.cs
+++ Assets/TriggerEvent.cs
@@ -6,8 +6,14 @@ using UnityEngine.Events;
 public class TriggerEvent : MonoBehaviour
 {
   public UnityEvent trigEvent;
+  public UnityEvent trigEvent2;
   void OnTriggerEnter2D(Collider2D col)
   {
     trigEvent.Invoke();
+    print("yes");
+  }
+  void OnTriggerExit2D(){
+    trigEvent2.Invoke();
+    print("no");
   }
 }
